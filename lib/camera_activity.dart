0891c96b54fa0a1c1e49dd03e063c2dcc721141d
23nobody <iit2018073@iiita.ac.in>
2021-03-06 02:50:46 +0530

    camera to file chain complete

--- lib/camera_activity.dart
+++ lib/camera_activity.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:vision_assistant/main.dart';
@@ -9,7 +11,7 @@ class CameraActivity extends StatefulWidget {
 
 class _CameraActivityState extends State<CameraActivity> {
   CameraController controller;
-
+  XFile imageFile;
   @override
   void initState() {
     super.initState();
@@ -18,6 +20,8 @@ class _CameraActivityState extends State<CameraActivity> {
       if (!mounted) {
         return;
       }
+      print(controller.value.flashMode);
+      print(controller.value.focusMode);
       setState(() {});
     });
   }
@@ -28,12 +32,14 @@ class _CameraActivityState extends State<CameraActivity> {
     super.dispose();
   }
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     if (!controller.value.isInitialized) {
       print("laaaaaaaaaaaaaaaaaaaaaaaaaa");
       return SafeArea(
         child: Scaffold(
+          key: _scaffoldKey,
           appBar: AppBar(
             title: Text('Flutter TTS'),
           ),
@@ -47,8 +53,130 @@ class _CameraActivityState extends State<CameraActivity> {
       );
     }
     print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-    return AspectRatio(
-        aspectRatio: controller.value.aspectRatio,
-        child: CameraPreview(controller));
+    return SafeArea(
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('Flutter TTS'),
+        ),
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            // child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: CameraPreview(controller),
+              ),
+            ),
+            // ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                _captureControlRowWidget(),
+                _thumbnailWidget(),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  /// Display the thumbnail of the captured image or video.
+  Widget _thumbnailWidget() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          imageFile == null
+              ? Container()
+              : SizedBox(
+                  child: Image.file(File(imageFile.path)),
+                  width: 64.0,
+                  height: 64.0,
+                ),
+        ],
+      ),
+    );
+  }
+
+  /// Display the control bar with buttons to take pictures and record videos.
+  Widget _captureControlRowWidget() {
+    final CameraController cameraController = controller;
+
+    return
+        // Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.max,
+        // children: <Widget>[
+        Expanded(
+      child: IconButton(
+        icon: const Icon(Icons.camera_alt),
+        color: Colors.blue,
+        onPressed: cameraController != null &&
+                cameraController.value.isInitialized &&
+                !cameraController.value.isRecordingVideo
+            ? onTakePictureButtonPressed
+            : null,
+      ),
+    )
+        // ],
+        // )
+        ;
+  }
+
+  void onTakePictureButtonPressed() {
+    takePicture().then((XFile file) {
+      if (mounted) {
+        setState(() {
+          imageFile = file;
+        });
+        if (file != null) showInSnackBar('Picture saved to ${file.path}');
+      }
+    });
+  }
+
+  Future<XFile> takePicture() async {
+    final CameraController cameraController = controller;
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return null;
+    }
+
+    if (cameraController.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      XFile file = await cameraController.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+  }
+
+  void showInSnackBar(String message) {
+    // ignore: deprecated_member_use
+    _scaffoldKey.currentState?.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void logError(String code, String message) {
+    if (message != null) {
+      print('Error: $code\nError Message: $message');
+    } else {
+      print('Error: $code');
+    }
   }
 }
